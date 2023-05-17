e294195e1c00ed482745a23ea5aca51bf5284619
Zelody <zelody@Equipo1.com>
2023-05-17 17:18:34 -0600

    modularizando

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import CategoriesList from "./CategoriesList"
+
+const Categories = ({categories = [], setCategories}) =>{
+
+    const [inputValue, setInputValue] = useState("")
+
+    const handleInput = ({target}) =>{
+        setInputValue(target.value)
+    }
+
+    const handleAddCategoryButtom = () =>{
+        if (!categories.includes(inputValue)){
+            setCategories([inputValue, ...categories])
+            setInputValue("")
+        }
+    }
+
+    return(
+        <>
+        <input 
+        onChange={(e)=> handleInput(e)}
+        placeholder="Write Category Name"
+        type="text"
+        value={inputValue} 
+        />
+        <button
+        className="btn btn-primary btn-sm ms-2 mb-1"
+        onClick={handleAddCategoryButtom}
+        type="button"
+        >
+        Add
+        </button>
+        <br />
+        <CategoriesList categories={categories}/>
+        </>
+    )
+}
+
+export default Categories
